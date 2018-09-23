0242d76a054cd3018a82d1d00329fd1eaaa863db
Owalid <othmane.el.ayadi@gmail.com>
2018-09-23 20:29:13 +0200

    finished thanks p5

--- sketch.js
+++ sketch.js
@@ -1,11 +1,15 @@
+var angle = 0;
+
 function    setup() {
     createCanvas(400, 400);
+    slider = createSlider(0, TWO_PI, PI / 4, 0.001);
 }
 
 function    draw() {
     background(51);
 
     translate(200, height);
+    angle = slider.value();
     stroke(255);
     branch(100);
 }
@@ -13,6 +17,14 @@ function    draw() {
 function    branch(len) {
     line(0, 0, 0, -len)
     translate(0, -len)
-    rotate(PI/4);
-    line(0, 0, 0, -len)
+    if (len > 4) {
+        push();
+        rotate(angle);
+        branch(len * 0.7)
+        pop();
+        push();
+        rotate(-angle);
+        branch(len * 0.7);
+        pop();
+    }
 }
